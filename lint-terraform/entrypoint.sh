00cf11112e28740314da5e88dbc83952bb1f2cf2
Demian Ginther <st.diluted@gmail.com>
2021-02-24 14:05:07 -0700

    fix syntax

--- lint-terraform/entrypoint.sh
+++ lint-terraform/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #configure git
-if [[ $GH_TOKEN ]]; then
+if [ ! -z "$GH_TOKEN" ]; then
   git config --global url."https://foo:${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
 fi
 
